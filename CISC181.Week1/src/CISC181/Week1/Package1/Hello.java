2c0cab49fe8f1f10f790874ad36854cc26027243
Bert.Gibbons <bgibbons@udel.edu>
2014-07-10 13:19:24 -0400

    Package 2

--- CISC181.Week1/src/CISC181/Week1/Package1/Hello.java
+++ CISC181.Week1/src/CISC181/Week1/Package1/Hello.java
@@ -7,6 +7,7 @@ package CISC181.Week1.Package1;
  * Note the { } curly braces.  It's a standard delimiter in Java.  One set of curly braces shows the scope of the class, another set of curly braces
  *      shows the scope of a given method.
  * Java is case-sensitive.  'System' is the not the same as 'system'.
+ * Statements are terminated by a semicolon (;)  
  * 
  * Things to consider:
  * What is a package?
