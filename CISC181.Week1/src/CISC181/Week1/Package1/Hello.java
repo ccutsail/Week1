9cd796d20f5cc30d794c18c3689ec91a1b849ccc
Bert.Gibbons <bgibbons@udel.edu>
2014-07-10 12:53:38 -0400

    Initial Commit

---
+++ CISC181.Week1/src/CISC181/Week1/Package1/Hello.java
@@ -0,0 +1,23 @@
+package CISC181.Week1.Package1;
+
+/*
+ * Notes - Not much to this one...  Simple 'main' method, prints a message to the console.  
+ * 
+ * Things to note:
+ * Note the { } curly braces.  It's a standard delimiter in Java.  One set of curly braces shows the scope of the class, another set of curly braces
+ *      shows the scope of a given method.
+ * Java is case-sensitive.  'System' is the not the same as 'system'.
+ * 
+ * Things to consider:
+ * What is a package?
+ * What does 'public' modifier mean?
+ * What does 'static' keyword do?
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+	}
+
+}
