42f60b032e288d12a13e1892182785f10d8de30f
Bert.Gibbons <bgibbons@udel.edu>
2014-07-10 14:42:01 -0400

    Added package 5

---
+++ CISC181.Week1/src/CISC181/Week1/Package4/CMain.java
@@ -0,0 +1,27 @@
+package CISC181.Week1.Package4;
+
+/*
+ * Notes - Introduction to numeric datatypes.  
+ * 
+ * Things to note:
+ * 
+ * All of the datatypes below are numeric.  I've assigned the number one to each variable.
+ * 
+ * Things to consider:
+ * Why bother having different data types?  Check out 2.9 in the text.  What is the difference between each of the datatypes below?
+ */
+
+public class CMain {
+
+	public static void main(String[] args) {
+
+		byte b = 1;
+		short s = 1;
+		int i = 1;
+		long l = 1;
+		float f = 1;
+		double d = 1;
+
+	}
+
+}
